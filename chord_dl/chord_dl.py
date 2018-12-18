7b0e9f6f5a212e1813d0d6ff9e4156911ec11e01
aikawa_yuduki <tama.pen.sula2240@gmail.com>
2018-12-18 14:28:55 +0900

    a

--- chord_dl/chord_dl.py
+++ chord_dl/chord_dl.py
@@ -11,30 +11,37 @@ i = pygame.midi.Input(input_id)
 
 def input_chord():
     chord = []
-    print("Press n to input next chord.")
+    print("Press n to input next chord. reset button:r")
     while True:
         if i.poll():
             midi_events = i.read(10)
             if midi_events[[[2]]] > 0:
                 chord.append(midi_events[[[1]]])
 
-        chord = list(set(chord))
-        print("\r temporary inputs: ",chord,end="")
         if msvcrt.kbhit():
             kb = msvcrt.getch()
             if kb.decode() == "n":
                 print("input chord: ",chord)
                 break
+            if kb.decode() == "r":
+                chord = []
+
+        chord = list(set(chord))
+        print("\r temporary inputs: ",chord,end="")
+                
+
 
     return chord
 
 def loop():
     chords = []
-    print("Press d to end a section.")
+    print("Press d to end a section. reset button:r")
     while True:
         chords.append(input_chord())
         if msvcrt.kbhit():
             kb = msvcrt.getch()
             if kb.decode() == "d":
                 break
+            if kb.decode() == "r":
+                chords = []
 
